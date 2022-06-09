b9df8a8cc36b9aa6e794027d610af493a1a48e27
Ricardo Ruiz <gaanzz11@gmail.com>
2022-06-08 19:28:52 -0500

    Final commit

--- pages/Components/Characters.tsx
+++ pages/Components/Characters.tsx
@@ -32,57 +32,68 @@ const Characters: NextPage = ({characters}: any) => {
               <div className="w-80">
                 <img src={image} className="rounded-full w-40 float-right" />
               </div>
-              {idx !== id ?
-              <div className="flex flex-col justify-between w-80 ml-12 h-48 items-left bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 p-6">
-                <p className="flex justify-between text-xs">
-                  <b>Name: </b>
-                  <i>{name}</i>
-                </p>
-                <p className="flex justify-between text-xs">
-                  <b>Status: </b>
-                  <i>{status}</i>
-                </p>
-                <p className="flex justify-between text-xs">
-                  <b>Gender: </b>
-                  <i>{gender}</i>
-                </p>
-                <p className="flex justify-between text-xs">
-                  <b>Episode: </b>
-                  <i className='underline text-blue-600' onClick={(e) => setIDX(id)}>{episode[0].name}</i>
-                </p>
-                <p className="flex justify-between text-xs">
-                  <b>Location: </b>
-                  <i>{location.name}</i>
-                </p>
-                <p className="flex justify-between text-xs">
-                  <b>Dimension: </b>
-                  <i>{location.dimension}</i>
-                </p>
-                <p className="flex justify-between text-xs">
-                  <b>Origin: </b>
-                  <i>{origin.name}</i>
-                </p>
-              </div> :
-              <div onClick={(e) => setIDX(-1)} className="flex flex-col justify-between overflow-scroll overflow-x-hidden w-80 ml-12 h-48 items-left bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 p-6">
-                <p className="flex justify-between text-xs">
-                  <b>Name: </b>
-                  <i>{episode[0].name}</i>
-                </p>
-                <p className="flex justify-between text-xs">
-                  <b>Episode: </b>
-                  <i>{episode[0].episode}</i>
-                </p>
-                <p className="flex justify-between text-xs">
-                  <b>Air Date: </b>
-                  <i>{episode[0].air_date}</i>
-                </p>
-                <ul>
-                  <b className='text-xs'>Characters: </b>
-                  {episode[0].characters.map(((character: any) => <li className='text-xs'>- {character.name}</li>))}
-                </ul>
-                <i className='rotate-180'>&#10137;</i>
-              </div>
-              }
+              {idx !== id ? (
+                <div className="flex flex-col justify-between w-80 ml-12 h-48 items-left bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 p-6">
+                  <p className="flex justify-between text-xs">
+                    <b>Name: </b>
+                    <i>{name}</i>
+                  </p>
+                  <p className="flex justify-between text-xs">
+                    <b>Status: </b>
+                    <i>{status}</i>
+                  </p>
+                  <p className="flex justify-between text-xs">
+                    <b>Gender: </b>
+                    <i>{gender}</i>
+                  </p>
+                  <p className="flex justify-between text-xs">
+                    <b>Episode: </b>
+                    <i
+                      className="underline text-blue-600"
+                      onClick={(e) => setIDX(id)}
+                    >
+                      {episode[0].name}
+                    </i>
+                  </p>
+                  <p className="flex justify-between text-xs">
+                    <b>Location: </b>
+                    <i>{location.name}</i>
+                  </p>
+                  <p className="flex justify-between text-xs">
+                    <b>Dimension: </b>
+                    <i>{location.dimension}</i>
+                  </p>
+                  <p className="flex justify-between text-xs">
+                    <b>Origin: </b>
+                    <i>{origin.name}</i>
+                  </p>
+                </div>
+              ) : (
+                <div
+                  onClick={(e) => setIDX(-1)}
+                  className="flex flex-col justify-between overflow-scroll overflow-x-hidden w-80 ml-12 h-48 items-left bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 p-6"
+                >
+                  <p className="flex justify-between text-xs">
+                    <b>Name: </b>
+                    <i>{episode[0].name}</i>
+                  </p>
+                  <p className="flex justify-between text-xs">
+                    <b>Episode: </b>
+                    <i>{episode[0].episode}</i>
+                  </p>
+                  <p className="flex justify-between text-xs">
+                    <b>Air Date: </b>
+                    <i>{episode[0].air_date}</i>
+                  </p>
+                  <ul>
+                    <b className="text-xs">Characters: </b>
+                    {episode[0].characters.map((character: any) => (
+                      <li className="text-xs">- {character.name}</li>
+                    ))}
+                  </ul>
+                  <i className="rotate-180">&#10137;</i>
+                </div>
+              )}
             </div>
           );
         })}
