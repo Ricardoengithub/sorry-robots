2ea725e0be69ccd71893436e869c5351b8255346
Ricardo Ruiz <gaanzz11@gmail.com>
2022-06-08 19:16:17 -0500

    Final commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import type { NextPage } from 'next'
 import { useState, useEffect } from 'react';
+import Characters from './Components/Characters';
 import fetchApi from './util/api';
 
 const Home: NextPage = () => {
   const [characters, setCharacters] = useState([])
+  const [search, setSearch] = useState("")
+  const [searchResult, setSearchResult] = useState([])
 
   useEffect(() => {
     async function fetchData() {
@@ -35,50 +38,25 @@ const Home: NextPage = () => {
 
       const { data } = await response.json();
       setCharacters(data.characters.results)
-      console.log(data.characters.results)
     }
     fetchData();
   }, []);
 
+  function handleChange(e:any) {
+    setSearch(e.target.value)
+    let newCharacters = characters.filter((character: any) => character.name.toLowerCase().includes(e.target.value.toLowerCase()))
+    setSearchResult(newCharacters)
+  }
+
   return (
-    <div className="container">
-      {characters.map((item, id) => {
-        const {name, gender, status, image, episode, location, origin}: {name: string, gender: string, status: string, image: string, episode: any, location: any, origin: any} = item
-        return (
-          <div key={id + name}>
-            <div>
-              <img src={image} />
-            </div>
-            <div>
-              <div>{name}</div>
-              <div>{status}</div>
-              <div>{gender}</div>
-              <div>
-                {episode[0].name} {episode[0].episode}
-              </div>
-              <div>{location.name}</div>
-              <div>{location.dimension}</div>
-              <div>{origin.name}</div>
-            </div>
-            <button
-              data-tooltip-target="tooltip-click"
-              data-tooltip-trigger="click"
-              type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Tooltip click
-            </button>
-            <div
-              id="tooltip-click"
-              role="tooltip"
-              className="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
-            >
-              Tooltip content
-              <div className="tooltip-arrow" data-popper-arrow></div>
-            </div>
-          </div>
-        );
-        })}
+    <div className="w-full">
+      <div className='text-center'>
+        <h1 className="text-5xl pt-12 pb-12">
+          Rick and Morty Test
+        </h1>
+        Search: <input type="text" onChange={(e) => handleChange(e)} value={search} className="rounded pl-1 outline-none"/>
+      </div>
+      <Characters characters={!search ? characters : searchResult} />
     </div>
   );
 }
